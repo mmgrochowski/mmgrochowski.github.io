e550dd93f35ce08fd92bbbad538e844039be3236
Marcin Grochowski <mmgrochowski@gmail.com>
2018-02-10 16:40:48 +0100

    zmienne

---
+++ JS/zmienne/js/2-zmienne.js
@@ -0,0 +1,13 @@
+//var kolorStronyMojejInternetowej;
+// zasada camelCase
+
+var imie = "Marcin";
+var nazwisko = "Grochowski";
+var wiek = "30";
+var wzrost;
+
+var wynik = "Imie: " + imie + " Nazwisko: " + nazwisko + " Wiek: " + wiek;
+console.log(imie);
+console.log(nazwisko);
+console.log(wiek);
+console.log(wynik);
