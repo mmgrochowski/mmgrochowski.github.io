0965245bad47675aec53ec5c800c56ef7cd5fc66
Marcin Grochowski <mmgrochowski@gmail.com>
2018-02-10 15:55:06 +0100

    comments

---
+++ JS/comments/1-comments.js
@@ -0,0 +1,10 @@
+/*
+
+komentarz blokowy
+-dzieki niemu komentujemy w wiecej niż jednej lini ctrl + shift +/
+
+*/
+
+// komentarz liniowy, komentarz kończy się wraz z linią wiersza nie zamykamy wystarczy enter ctrl +
+to już nie jest komentowane
+
