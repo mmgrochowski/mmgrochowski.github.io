a14c74d53d5d2d69742f2738b88f89a3d426ce80
Marcin Grochowski <mmgrochowski@gmail.com>
2018-02-11 10:15:29 +0100

    funkcje

---
+++ JS/funkcje/js/3-funkcje.js
@@ -0,0 +1,25 @@
+function obliczWynik() {
+    var wynik = 10 * 15 * 20;
+    return wynik;
+}
+
+var wynikObliczenia = obliczWynik();
+//console.log(wynikObliczenia)
+
+
+//
+//
+
+function obliczWynik(paramert1, paramert2, paramert3) {
+    var wynik = paramert1 * paramert2 * paramert3;
+    return wynik;
+}
+
+var wynikObliczenia = obliczWynik(20, 10, 3);
+console.log(wynikObliczenia);
+
+var wynikObliczenia2 = obliczWynik(10, 15, 4);
+console.log(wynikObliczenia2);
+
+var wynikObliczenia3 = obliczWynik(30, 10, 5);
+console.log(wynikObliczenia3);
