b75380490e393a1737a44ed124ee0af6b7816723
Marcin Grochowski <mmgrochowski@gmail.com>
2018-02-11 11:41:38 +0100

    tablice

---
+++ JS/tablice/js/4-tablice.js
@@ -0,0 +1,53 @@
+var imiona = ["Paweł" , "Krzysztof" , "Kasia" , "Nikola" , "Kamil"];
+//console.log(imiona[2]);
+
+//console.log(imiona);
+//dodawanie do tablicy zindeksowane
+//imiona[5] = "Monika";
+//imiona[6] = "Marcin";
+
+
+//console.log(imiona);
+
+//push dodaje imiona na końcu tablicy
+
+//imiona.push("Mikołaj"); // dodaje do tabeli !! na ostatnim miejscu !!
+
+//console.log(imiona);
+
+//imiona.pop(); //usuwa ostatni element z tablicy
+
+
+// wyświetla usuniety element
+//var usunietyElement= imiona.pop();
+//console.log(usunietyElement);
+
+
+//wstawia na początku tablicy
+//imiona.unshift("Kasia" , "Michał");
+
+
+// usuwanie elemanty z początku tablicy
+//imiona.shift();
+
+//sprawdza ilość elemantów w tablicy
+
+//console.log(imiona.length);
+
+//łączy elementy z tablicy
+
+//var tablicaJakoTekst = imiona.join("--------");
+//console.log(tablicaJakoTekst);
+
+
+//odwrócenie wartości tabeli
+
+var imionaOdwrocone = imiona.reverse();
+
+console.log(imionaOdwrocone);
+
+//sortowanie sablicy
+
+var posortowana = imiona.sort();
+
+console.log(posortowana);
