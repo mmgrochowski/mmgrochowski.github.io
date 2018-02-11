ed113c9902c36487948144c5c07a7a52f05cafdb
Marcin Grochowski <mmgrochowski@gmail.com>
2018-02-11 15:23:21 +0100

    9.0

---
+++ JS/operatory/js/9-operatory.js
@@ -0,0 +1,79 @@
+//var liczba1 = 13;
+//var liczba2 = 6;
+//var prawda = true;
+
+
+//console.log( liczba1 * liczba2);
+//console.log( liczba1 / liczba2);
+//console.log( liczba1 + liczba2);
+//console.log( liczba1 - liczba2);
+//console.log( liczba1 % liczba2);
+
+//liczba1++;
+//console.log(liczba1);
+
+//liczba2--;
+//console.log(liczba2);
+
+//console.log(liczba1++);//najpierw consolog potem dodaje
+//console.log(++liczba1);//najpierw dodaje potem consolog
+
+
+//operatory przypisania
+
+//liczba1 +=2; // liczba1= liczba1 + 2 
+//liczba1 -=10; // liczba1- licza1-10
+//liczba1 %=6;
+
+//operatory warunkowe s35
+
+//var wynik = (liczba1 <= liczba2) ? "liczba1 wieksza od liczba2 " : "liczba1 mniejsza od liczba2"
+//var wynik = (liczba1 == liczba2) ? " prawda " : " fałsz "
+
+
+//var pietro =4;
+
+//if(liczba1 > liczba2 && pietro == 14) {
+ //   console.log("spełnione zostały dwa warunki , takża true"}
+
+
+//!przed argumentem - zmienia sens logiczny
+
+
+//if(liczba1 == liczba2 ) 
+//{ console.log("liczby są równe");}
+//else if(liczba1 < liczba2 ) 
+//{ console.log("liczba2 jest większa od liczby1");  }
+//else if( liczba1 == "smog")
+//{ console.log("liczba1równa się smog"); }
+//else
+//{ console.log("kod, który wykona się gdy żaden z warunków nie będzie wykonany");  }
+
+// pentla switch
+//var grupa = 30;
+//
+//switch( grupa ) {
+//    case 10:
+//        console.log("liczba osób w grupie = 10");
+//        break;
+//    case 12:
+//        console.log("liczba osób w grupie - 12");
+//        break;
+//    case 14:
+//        console.log("liczba osób w grupie - 14");
+//        break;
+//    case 16:
+//        console.log("liczba osób w grupie - 16");
+//        break;
+//    case 18:
+//        console.log("liczba osób w grupie - 18");
+//        break;
+//    case 20:
+//        console.log("liczba osób w grupie - 20");
+//        break;
+//    default:
+//        console.log("nie wiadomo ile osób w grupe");
+//}
+
+
+
