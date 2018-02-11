e2a09bc72d0343a0ad36d23fafe5591f4e34e268
Marcin Grochowski <mmgrochowski@gmail.com>
2018-02-11 16:36:54 +0100

    petle1

---
+++ JS/pętle/js/10-petle.js
@@ -0,0 +1,41 @@
+// for ( wyrażenie początkowe ; wyrażenie warunkowe ; wyrażenie modyfikujące ) {  // instrukcje do wykonania }
+
+
+//var ileRazy = 30;
+//
+//for( var i =0; i < ileRazy; i++) {
+//    var iteracja = i +1;
+//    console.log("Iteracja pętli nr" + iteracja );
+//}
+
+//var tablica =["audi", "mazda", "bmw", "ford", "fiat"];
+
+
+//for( var i = 0; i< tablica.length; i++) {
+//    console.log("tablica! index = " + i+ ", wartość= " + tablica[i]);
+//}
+
+//
+//tablica.forEach( function( elementTablicy,indexTablicy) {
+//    if( elementTablicy == "mazda" )
+//    console.log(" index: " + indexTablicy + " ,wartość: " + elementTablicy);
+//} );
+
+
+//pętla while sprawdza określoną ilośc razy
+
+//var iter = 0;
+//while(iter <20) {
+//      console.log(iter );
+//    ++iter; }
+//
+
+// do while wykonuje się minimum raz niezależnie od spełnienia warunku
+
+//var iter = 20;
+//
+//do{
+//    console.log(iter);
+//    ++iter;
+//} while(iter<10);
+
