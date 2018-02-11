0627f6689e0f2515a75e7e24d2c9b9167f5a72dc
Marcin Grochowski <mmgrochowski@gmail.com>
2018-02-11 13:51:21 +0100

    obiekty-matemrtyczne

---
+++ JS/obiekt.math/js/8-obiekt-math.js
@@ -0,0 +1,8 @@
+console.log(Math.E);
+console.log(Math.PI);
+console.log(Math.LN10);
+console.log(Math.cos(90));
+console.log(Math.pow(2,6));
+console.log(Math.random());
+console.log(Math.ceil(1.7));
+console.log(Math.floor(1.7));
