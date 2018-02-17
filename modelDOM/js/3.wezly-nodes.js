f8cf6fa9500370ce6e791d4d2725ab6abc5642a3
Marcin Grochowski <mmgrochowski@gmail.com>
2018-02-17 12:44:27 +0100

    3

---
+++ modelDOM/js/3.wezly-nodes.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var pierwszyDiv = document.getElementById('parFirst');
+//console.log(pierwszyDiv);
+//console.log(pierwszyDiv.parentNode);
+//console.log(pierwszyDiv.parentElement);
+//console.log(pierwszyDiv.childNodes);
+//console.log(pierwszyDiv.childNodes[1]);
+//console.log(pierwszyDiv.children);
+//console.log(pierwszyDiv.firstChild);
+//console.log(pierwszyDiv.lastChild);
+//var pierwszyChild = pierwszyDiv.childNodes[0];
+//console.log(pierwszyChild);
+//console.log(pierwszyChild.nextSibling);
+
+var childNodesDiva = pierwszyDiv.childNodes;
+
+childNodesDiva.forEach(function(element){
+    if (element.nodeType ==3) {
+        console.log(element);
+    }
+}
+);
