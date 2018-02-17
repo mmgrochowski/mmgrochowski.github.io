780c8e1b47e9fa4f2bf8398284646eb5346689d6
Marcin Grochowski <mmgrochowski@gmail.com>
2018-02-17 12:01:12 +0100

    2

---
+++ modelDOM/js/1.modelDOM.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log (window);
+console.log (window.innerHeight);
+console.log (window.outerHeight);
+
+console.log(document);
---
+++ modelDOM/js/2.znajdowanieElementow.js
@@ -0,0 +1,22 @@
+"use strict";
+
+//var parFirst = document.getElementById('parFirst');
+//console.log(parFirst);
+////po id
+//
+//var linki = document.getElementsByClassName('superlink');
+//console.log(linki);
+//
+////po klasie
+
+//var linkiPoTagu = document.getElementsByTagName('a');
+//console.log(linkiPoTagu);
+
+//var divPoId = document.querySelector('#parSecond');
+//console.log(divPoId);
+
+var pierwszyLinkPoSelrktorze = document.querySelector('.superlink');
+console.log(pierwszyLinkPoSelrktorze);
+
+var linkiPoSelektorze = document.querySelectorAll('.superlink');
+console.log(linkiPoSelektorze);
