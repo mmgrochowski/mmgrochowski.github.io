3017a4b1856a04e50ff80bd407f50aaccab8d804
Marcin Grochowski <mmgrochowski@gmail.com>
2018-03-14 17:31:56 +0100

    14.03

---
+++ zadania-domowe/zadanieAjax/main.js
@@ -0,0 +1,11 @@
+  'use strict';
+
+
+
+  $('#pobierz').click(function () {
+      $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function (data) {
+          $('body').append('<div>Imie = ' + data.imie + '<br>' + ' Nazwisko = ' + data.nazwisko + '<br>' + ' Zawód = ' + data.zawod + '<br>' + ' Firma = ' + data.firma + '</div>');
+          
+          $("div").addClass("dane-programisty");
+      });
+  })
